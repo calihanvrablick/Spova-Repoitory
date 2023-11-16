59b42e46b583ddf4eb8f3655eb597e973ec9bb8f
lamomano <kanyonsr@gmail.com>
2023-11-16 08:59:04 -0800

    added player character with camera and physical movement

---
+++ Spova1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,177 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TextCore.Text;
+
+/*
+ * Author: [Nguyen, Kanyon & Vrablick, Calihan]
+ * Last Updated: [11/16/2023]
+ * [Handles the camera movement and physical movement of the player]
+ */
+public class PlayerMovement : MonoBehaviour
+{
+    // configurables
+
+    public float sensitivity = 3.0f;
+    public float smoothing = 2.0f;
+
+
+
+    // playerholders
+
+    Vector2 mouseLook;
+    Vector2 smoothV;
+
+    public GameObject character;
+    private Camera cameraObject;
+
+    //A boolean value that will tell you if you are within .1 Unity Unit from the ground
+    private bool is_grounded;
+    //The rigid body attached to the player
+    private Rigidbody body;
+    //how close are we to a wall on our left side
+    private float distance_to_wall_left = 2f;
+    //how close are we to a wall on our right side
+    private float distance_to_wall_right = 2f;
+    //how close are we to a wall going forward
+    private float distance_to_wall_forward = 2f;
+    //how close are we to a wall going backwards
+    private float distance_to_wall_back = 2f;
+
+    //Start is a function that is called once when the object is Instatiated. 
+    void Start()
+    {
+        // set up camera
+        cameraObject = character.transform.Find("Main Camera").GetComponent<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is a function that is called once per frame
+    void Update()
+    {
+
+        CameraUpdate();
+
+        float translation = Input.GetAxis("Vertical") * 10 * Time.deltaTime;
+        float straffe = Input.GetAxis("Horizontal") * 10 * Time.deltaTime;
+
+        if (!is_grounded)
+            straffe /= 2;
+
+        if (!is_grounded)
+            translation /= 2;
+
+
+        //if I'm too close to a wall, don't go that direction anymore
+        if ((distance_to_wall_back <= .6 && translation < 0) || (distance_to_wall_forward <= .6 && translation > 0))
+        {
+            translation = 0;
+        }
+        if ((distance_to_wall_right < .6 && straffe > 0) || (distance_to_wall_left < .6 && straffe < 0))
+        {
+            straffe = 0;
+        }
+
+        //Translate to move.
+        transform.Translate(straffe, 0, translation);
+
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+
+    /// <summary>
+    /// called whenever update is called
+    /// handles the functionality of the player's camera
+    /// </summary>
+    private void CameraUpdate()
+    {
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        mouseLook += smoothV;
+
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
+        //s mouseLook.x = Mathf.Clamp(mouseLook.x, -90f, 90f);
+
+        cameraObject.transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+
+
+
+    private void FixedUpdate()
+    {
+        DistanceToWall();
+        Vector3 oldRot = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(0, oldRot.y, 0);
+    }
+
+    private void DistanceToWall()
+    {
+        RaycastHit hit;
+        Ray left_ray = new Ray(transform.position, -transform.right);
+        Ray front_ray = new Ray(transform.position, transform.forward);
+        Ray back_ray = new Ray(transform.position, -transform.forward);
+        Ray right_ray = new Ray(transform.position, transform.right);
+
+        //Raycast left to see if I find a wall
+        if (Physics.Raycast(left_ray, out hit) && !hit.collider.isTrigger)
+        {
+            distance_to_wall_left = hit.distance;
+        }
+        else
+        {
+            distance_to_wall_left = 3;
+        }
+
+        //Raycast center forward to find a wall
+        if (Physics.Raycast(front_ray, out hit) && !hit.collider.isTrigger)
+        {
+            distance_to_wall_forward = hit.distance;
+        }
+        else
+        {
+            distance_to_wall_forward = 3;
+        }
+
+        //Raycast center forward to find a wall
+        if (Physics.Raycast(back_ray, out hit) && !hit.collider.isTrigger)
+        {
+            distance_to_wall_back = hit.distance;
+        }
+        else
+        {
+            distance_to_wall_back = 3;
+        }
+
+        //Raycast right to find a wall
+        if (Physics.Raycast(right_ray, out hit) && !hit.collider.isTrigger)
+        {
+            distance_to_wall_right = hit.distance;
+        }
+        else
+        {
+            distance_to_wall_right = 3;
+        }
+
+
+        //Raycast down to find the ground
+        if (Physics.Raycast(transform.position, -transform.up, 1.1f))
+        {
+            is_grounded = true;
+        }
+        else
+        {
+            is_grounded = false;
+        }
+    }
+}
