80ad7f30823adccc505b604be285b5e7afdd0f3e
lamomano <kanyonsr@gmail.com>
2023-11-29 15:50:22 -0800

    new player + WIP gui

--- Spova1/Assets/Scripts/PlayerHandler.cs
+++ Spova1/Assets/Scripts/PlayerHandler.cs
@@ -2,8 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCollisions : MonoBehaviour
+/*
+ * Author: [Nguyen, Kanyon] & [Vrablick, Calihan]
+ * Last Updated: [11/29/2023]
+ * [Holds player variables and handles the collisions for the player.]
+ */
+public class PlayerHandler : MonoBehaviour
 {
+
+    public int health;
+    public int Coins;
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Spova1/Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+
+/*
+ * Author: [Nguyen, Kanyon] & [Vrablick, Calihan]
+ * Last Updated: [11/13/2023]
+ * [Handles all the scores and lives gui in the game scene.]
+ */
+public class UIManager : MonoBehaviour
+{
+
+    public PlayerHandler playerHandler;
+    public TMP_Text healthText;
+    public TMP_Text coinsText;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = "Health: " + playerHandler.health;
+        coinsText.text = "Coins: " + playerHandler.Coins;
+    }
+}
