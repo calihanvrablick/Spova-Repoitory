410f316578d4cc767c23005f6efea76c609b6732
calihanvrablick <cvrablick@csuchico.edu>
2023-12-08 19:54:19 -0800

    have this

---
+++ Spova1/Assets/Scripts/Lightsaber.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author: [Vrablick, Calihan] & [Nguyen, Kanyon]
+ * Last Updated: [12/08/2023]
+ * [Controls the damage of the lightsaber]
+ */
+
+public class Lightsaber : MonoBehaviour
+{
+    public int damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
